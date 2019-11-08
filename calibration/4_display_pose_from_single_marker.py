75f96a5f13f82f6fb0d0111f398f3497b6dbe796
Dennis Baldwin <dennisbaldwin@gmail.com>
2019-11-08 11:15:37 -0600

    Working towards getting distance from center of camera so we can yaw

--- calibration/4_display_pose_from_single_marker.py
+++ calibration/4_display_pose_from_single_marker.py
@@ -1,8 +1,9 @@
 import cv2
 from cv2 import aruco
 import numpy as np
+import math
 
-marker_length = .04
+marker_length = .04 # 4 cm
 aruco_dict = aruco.Dictionary_get(aruco.DICT_ARUCO_ORIGINAL)
 aruco_params =  aruco.DetectorParameters_create()
 
@@ -13,9 +14,12 @@ camera_matrix = np.array([[929.13251611,   0.,         479.17562521],
 distortion_coefficients = np.array([[ 1.35915086e-01, -2.23009579e+00, -1.37639118e-02, -2.29458613e-03,
    8.38818104e+00]])
 
+axis = np.array([[marker_length,0,0], [0,marker_length,0], [0,0,marker_length*-1]]).reshape(-1,3)
+
 # Get the video stream
 #camera = cv2.VideoCapture(0) # webcam
-camera = cv2.VideoCapture('udp://127.0.0.1:11111') # Tello
+#camera = cv2.VideoCapture('udp://127.0.0.1:11111') # Tello video from stream
+camera = cv2.VideoCapture('../videos/tello.avi') # Tello video from file
 
 while True:
     ret, img = camera.read()
@@ -25,16 +29,55 @@ while True:
 
     if ids is not None:
         img_aruco = aruco.drawDetectedMarkers(img, marker_corners, ids, (0, 255, 0))
+
+        # tvec is the center of the marker in the camera's world
         rvec, tvec, _ = aruco.estimatePoseSingleMarkers(marker_corners, marker_length, camera_matrix, distortion_coefficients)
+
+        print(tvec)
 
         # In case there are multiple markers
         for i in range(ids.size):
             img_aruco = aruco.drawAxis(img_aruco, camera_matrix, distortion_coefficients, rvec[i], tvec[i], marker_length)
 
-        #img_aruco = cv2.line(img_aruco, (0, 0), (int(x*10), int(y*10)), (0,0,255), 3)
+        
+        if rvec.size == 3:
+            imgpts, _ = cv2.projectPoints(axis, rvec, tvec, camera_matrix, distortion_coefficients)
+
+            p1 = (int(imgpts[0][0][0]), int(imgpts[0][0][1]))
+                
+            if p1 is not None:
+                cv2.circle(img_aruco, p1, 5, (0, 0, 255), -1)
+
+            p2 = (int(imgpts[1][0][0]), int(imgpts[1][0][1]))
+
+            if p2 is not None:
+                cv2.circle(img_aruco, p2, 5, (0, 255, 0), -1)
+
+            p3 = (int(imgpts[2][0][0]), int(imgpts[2][0][1]))
+            
+            if p3 is not None:
+                cv2.circle(img_aruco, p3, 5, (255, 0, 0), -1)
+
+            tvec_x = tvec[0][0][0]
+            tvec_y = tvec[0][0][1]
+            tvec_z = tvec[0][0][2]
+
+            # Distance from camera
+            distance = math.sqrt(tvec_x*tvec_x + tvec_y*tvec_y + tvec_z*tvec_z)
+
+
+        #for p in marker_corners:
+
+            # Draw line from center of camera to first corner
+            #cv2.arrowedLine(img_aruco, (480, 360), (int(p[0][0][0]), int(p[0][0][1])), (0, 0, 255), 3)
+
+
+        #print(x, ":", y)
+
+        #img_aruco = cv2.line(img_aruco, (0, 0), (int(x), int(y)), (0,0,255), 3)
 
         #print("rvec: ", rvec)
-        print("tvec: ", tvec)
+        #print("tvec: ", tvec)
 
     else:
         img_aruco = img
