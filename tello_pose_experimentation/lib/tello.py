fa74af1bbc1c39efa599fd8aea5f707739be13d2
Dennis Baldwin <dennisbaldwin@gmail.com>
2019-12-18 14:24:22 -0600

    Basic Tello class

---
+++ tello_pose_experimentation/lib/tello.py
@@ -0,0 +1,18 @@
+import socket
+
+class Tello:
+
+    def __init__(self):
+        self.port = 8889
+        self.addr = ('192.168.10.1', self.port)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    def send(self, message):
+        try:
+            self.sock.sendto(message.encode(), self.addr)
+            print("Sending message: " + message)
+        except Exception as e:
+            print("Error sending: " + str(e))
+
+    def receive(self):
+        return
