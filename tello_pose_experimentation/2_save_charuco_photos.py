7f03959969ce524afd0de8f12dd241929998a45a
Dennis Baldwin <dennisbaldwin@gmail.com>
2019-12-18 13:40:56 -0600

    Comments

--- tello_pose_experimentation/2_save_charuco_photos.py
+++ tello_pose_experimentation/2_save_charuco_photos.py
@@ -1,21 +1,41 @@
 import cv2
 import os
 import time
+from lib.tello import Tello
 
+# Construct a Tello instance so we can communicate with it over UDP
+tello = Tello()
 
+# Send the command string to wake Tello up
+tello.send("command")
+
+# Delay
+time.sleep(1)
+
+# Initialize the video stream which will start sending to port 11111
+tello.send("streamon")
+
+# Path for storing file
 file_path = os.getcwd()
-#cap = cv2.VideoCapture(0) # webcam
-cap = cv2.VideoCapture('udp://127.0.0.1:11111') # Tello video from stream
-#cap = cv2.VideoCapture('../videos/tello.avi') # Tello video from file
 
+# Start the video capture
+cap = cv2.VideoCapture('udp://127.0.0.1:11111')
+
+# Loop and grab image frames
 while True:
+
+    # Grab frame
     ret, frame = cap.read()
 
-    cv2.imshow('camera', frame)
+    # Show frame
+    cv2.imshow('Tello', frame)
 
+    # Press q key to exit
     key = cv2.waitKey(1) & 0xFF
     if key == ord('q'):
         break
+
+    # Press space bar to take photo
     if key == ord(' '):
         file_name = time.strftime("%Y%m%d-%H%M%S")
         cv2.imwrite(file_path + "/" + file_name + ".jpg", frame)
